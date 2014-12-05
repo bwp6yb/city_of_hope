f8b88eebd6d2e72679adf14ba7dbff242f0dee45
Luke Parham <lappp9@gmail.com>
2014-12-04 18:58:33 -0600

    mail stuff seems to be in order

---
+++ app/mailers/leader_mailer.rb
@@ -0,0 +1,12 @@
+class LeaderMailer < ActionMailer::Base
+  include SendGrid
+
+  sendgrid_category :use_subject_lines
+
+  default to: "lappp9@gmail.com"
+  #default to: "ccfjoe@juno.com"
+
+  def community_email(email_info)
+    mail(subject: "#{email_info[:name]} wants information on community groups", from: email_info[:email_address]) 
+  end
+end
