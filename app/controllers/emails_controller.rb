812756eac1fdffcd767ca7a330201adaeae59ccb
Brandt Parker <bwp6yb@mail.missouri.edu>
2014-12-10 15:33:17 -0600

    Email Form - Service

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,10 @@
 class EmailsController < ApplicationController
+
   def create
     LeaderMailer.community_email(params[:email]).deliver()
+  end
+
+  def create
+  	LeaderMailer.service_email(params[:email]).deliver()
   end
 end
--- app/mailers/leader_mailer.rb
+++ app/mailers/leader_mailer.rb
@@ -9,4 +9,8 @@ class LeaderMailer < ActionMailer::Base
   def community_email(email_info)
     mail(subject: "#{email_info[:name]} wants information on community groups", from: email_info[:email_address]) 
   end
+
+  def service_email(email_info)
+  	mail(subject: "Emailer wants information on service groups", from email_info[:email_address])
+  end
 end
